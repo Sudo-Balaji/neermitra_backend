9b20d3ac89d5acd943b6d0bd9c5ecfd1b04c5149
Sudo-Balaji <sbalaji66@yahoo.com>
2025-11-22 17:24:55 +0530

    inital project setup

---
+++ src/main/java/com/neermitra/neermitra_backend/NeermitraBackendApplication.java
@@ -0,0 +1,15 @@
+package com.neermitra.neermitra_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeermitraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeermitraBackendApplication.class, args);
+		
+		System.out.println("Neermitra !!");
+	}
+
+}
---
+++ src/test/java/com/neermitra/neermitra_backend/NeermitraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neermitra.neermitra_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeermitraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
